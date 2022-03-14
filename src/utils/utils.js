f92cca0b426827e12e105a81e1f3b4ab40ca143a
wandji20 <wbertrand10@gmail.com>
2022-03-14 08:06:17 +0100

    Add methods to save and retrieve jwt token with sessionStorage

---
+++ src/utils/utils.js
@@ -0,0 +1,8 @@
+export const setAuthToken = (token) => {
+  sessionStorage.setItem('quizCenterAuthToken', token);
+};
+
+export const getAuthToken = () => {
+  const token = sessionStorage.getItem('quizCenterAuthToken')
+  return token;
+};
