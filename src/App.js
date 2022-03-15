078caa071a6ebce36feaebbe1685e8a2ea64dcba
wandji20 <wbertrand10@gmail.com>
2022-03-15 01:46:30 +0100

    Add display for notifications

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
 import Nav from './components/header/Nav';
+import Notification from './components/notification/Notification';
+import { NotificationContext } from './context/notifications/NotificationContextProvider';
 
 function App() {
+  const { notice, alert } = useContext(NotificationContext);
   return (
     <>
       <Nav />
       <section id="content">
         <Outlet />
+        {
+          notice && <Notification message={notice} messageType="notice" />
+        }
+        {
+          alert && <Notification message={alert} messageType="alert" />
+        }
       </section>
     </>
   );
